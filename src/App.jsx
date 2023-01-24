cbbbc8f6b80054a1daf105fad4be4dff8c36889d
Manuel <nanoprozzi@gmail.com>
2023-01-24 00:44:02 -0300

    Breadcrumbs, forms & navigate component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider} f
 import Home from './pages/Home'
 import About from './pages/About'
 import Faq from './pages/help/Faq'
-import Contact from './pages/help/Contact'
+import Contact, { contactAction } from './pages/help/Contact'
 import NotFound from './pages/NotFound'
 import Careers, { careersLoader } from './pages/careers/Careers'
 import CareerDetails, { careerDetailsLoader } from './pages/careers/CareerDetails'
@@ -24,7 +24,7 @@ const router = createBrowserRouter(
 
         <Route path="help" element={<HelpLayout/>}>
           <Route path="faq" element={<Faq/>}/>
-          <Route path="contact" element={<Contact/>}/>
+          <Route path="contact" element={<Contact/>} action={contactAction}/>
         </Route>
 
         <Route path="careers" element={<CareersLayout/>} errorElement={<CareersError />}>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react"
+import { Navigate } from "react-router-dom"
+
 export default function Home() {
+    const [user, setUser] = useState('mario')
+
+    if (!user) {
+        return <Navigate to='/' replace={true}/>
+    }
+
     return (
         <div className="about">
             <h2>About Us</h2>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad ea minus cupiditate odio error fugit officiis nobis quasi reprehenderit, eligendi ullam, exercitationem sapiente, voluptate at perferendis? Voluptates doloremque expedita nulla.</p>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad ea minus cupiditate odio error fugit officiis nobis quasi reprehenderit, eligendi ullam, exercitationem sapiente, voluptate at perferendis? Voluptates doloremque expedita nulla.</p>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad ea minus cupiditate odio error fugit officiis nobis quasi reprehenderit, eligendi ullam, exercitationem sapiente, voluptate at perferendis? Voluptates doloremque expedita nulla.</p>
+            <button onClick={() => setUser(null)}>Logout</button>
         </div>
     )
 }
